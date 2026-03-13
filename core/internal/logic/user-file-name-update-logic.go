60cdaf23e1cdde516a4d0a895f74d6d27b3c5c48
lvxiaobai <386244641@qq.com>
2026-03-13 14:24:28 +0800

    新增查询用户文件名修改完善

--- core/internal/logic/user-file-name-update-logic.go
+++ core/internal/logic/user-file-name-update-logic.go
@@ -6,6 +6,7 @@ package logic
 import (
 	"cloud_disk/core/internal/models"
 	"context"
+	"errors"
 
 	"cloud_disk/core/internal/svc"
 	"cloud_disk/core/internal/types"
@@ -28,6 +29,15 @@ func NewUserFileNameUpdateLogic(ctx context.Context, svcCtx *svc.ServiceContext)
 }
 
 func (l *UserFileNameUpdateLogic) UserFileNameUpdate(req *types.UserFileNameUpdateRequest, userIdentity string) (resp *types.UserFileNameUpdateResponse, err error) {
+	//当当前目录存在与修改名字相同的文件时，不允许修改
+	count, err := l.svcCtx.Engine.Where("name=? AND parent_id=(select parent_id from user_repository where user_identity = ? and identity =?)", req.Name, userIdentity, req.Identity).Count(&models.UserRepository{})
+	if err != nil {
+		return nil, err
+	}
+	if count > 0 {
+		return nil, errors.New("该文件名已经存在")
+	}
+
 	up := models.UserRepository{
 		Name: req.Name,
 	}
